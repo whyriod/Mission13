97ca6fde0e8f0282695eebbfa1167a2bd1933f66
whyriod <whyriod@byu.edu>
2022-03-30 22:47:44 -0600

    Big push

---
+++ Mission13/Components/BowlerFilterViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using Mission13.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Components
+{
+    public class BowlerFilterViewComponent : ViewComponent
+    {
+        private IBowlersRepository _repo { get; set; }
+
+        public BowlerFilterViewComponent(IBowlersRepository b)
+        {
+            _repo = b;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            var types = _repo.Teams
+                .Select(x => x.TeamName)
+                .Distinct()
+                .OrderBy(x => x);
+            return View(types);
+        }
+    }
+}
